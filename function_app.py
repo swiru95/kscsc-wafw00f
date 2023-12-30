11c25046c218c0098256f6dd5639b7ba11859f3c
swiru95 <24733214+swiru95@users.noreply.github.com>
2023-12-30 21:56:51 +0100

    Working commit

---
+++ function_app.py
@@ -0,0 +1,37 @@
+import azure.functions as func
+import logging, json
+from wafw00f.main import WAFW00F
+
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="trigger_waf_woof")
+def trigger_waf_woof(req: func.HttpRequest) -> func.HttpResponse:
+    
+    #Check if POST
+    if req.method!="POST":
+        print(f"{req.method} is not valid.")
+        #return func.HttpResponse(f"Not Found", status_code=404)
+
+    #Check target param
+    url=req.params.get("target")
+    if(not url or not url.startswith('http')):
+         return func.HttpResponse(f"Bad Request",status_code=400)
+    
+    #create target JSON
+    target={'target':url,'status':'protected','solution':'none'}
+    
+    attacker = WAFW00F(target['target'],debuglevel=40)
+    
+    #Target ONLINE?
+    if attacker.rq is None:
+        target['status']='down'
+        
+    waf=attacker.identwaf(findall=True)
+    if len(waf) > 0:
+        target['solution']=waf[0]
+    elif (attacker.genericdetect()):
+        target['solution']='generic'        
+    else:
+        target['status']='unknown'
+         
+    return func.HttpResponse(json.dumps(target),status_code=200)
--- wafw00f/__init__.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-__version__ = '2.2.0'
-__license__ = 'BSD 3-Clause'
